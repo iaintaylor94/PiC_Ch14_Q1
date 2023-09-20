4d24a4c46e4438609defa3f57cfd6d3a34352d2e
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-20 13:35:38 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+// program to demonstrate the use of a pointer to a function
+
 #include <stdio.h>
 
+// Declare Function
+void functionPointer (int);
+
+// Define Types
+typedef void (*FunctionPtr) (int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  FunctionPtr instance = &functionPointer;
+
+  instance (100);
+  instance (200);
+  
   return 0;
+}
+
+// Define Function
+void functionPointer (int n) {
+  printf ("%d\n", n);
 }
